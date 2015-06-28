683a59584c9f232dd3cf12b825b9d11130c0c4d6
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2015-06-28 22:13:57 +0300

    add RemoteConsole

--- src/main/java/jk2serverbrowser/RemoteConsole.java
+++ src/main/java/jk2serverbrowser/RemoteConsole.java
@@ -1,72 +1,120 @@
 
 package jk2serverbrowser;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import udp.Connection;
 
 /**
  *
  * @author Markus Mulkahainen
  */
-public class RemoteConsole extends JDialog {
+public final class RemoteConsole extends JDialog {
     
-    private final Connection connection = new Connection();
+    private final RemoteConsoleController controller;
     
-    public RemoteConsole(JFrame parent) {
+    private JPasswordField password;
+    private JTextField ip;
+    private JTextField port;
+    private JTextArea area;
+    
+    public RemoteConsole(JFrame parent, RemoteConsoleController controller, String withIp, String withPort) {
         super(parent, "Remote Console", true);
         
-        getContentPane().add(createContent());
+        this.controller = controller;
+        
+        getContentPane().setLayout(new BorderLayout());
+                
+        setPreferredSize(new Dimension(800,600));
+        setLocation(parent.getLocation().x + 300, parent.getLocation().y + 200);
+        
+        getContentPane().add(createTopBar(withIp, withPort), BorderLayout.NORTH);
+        getContentPane().add(createTextArea(), BorderLayout.CENTER);
+        getContentPane().add(createBottomPanel(), BorderLayout.SOUTH);
+        
+        controller.response().subscribe(response -> { 
+            if (response.isEmpty()) return;
+            area.append(response);
+        });
         
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         pack();
         setVisible(true);
     }
     
-    public JPanel createContent() {
-        JPanel panel = new JPanel();
+    public JPanel createBottomPanel() {
+        JPanel bottom = new JPanel(new BorderLayout());
         
-        JButton button = new JButton("Send");
-        button.addActionListener(x -> {
-            try (DatagramSocket server = new DatagramSocket()) {
-                String str = "rcon suomi21 status";
-                byte[] msg = new byte[] { (byte) 0xff, (byte)0xff, (byte)0xff, (byte)0xff };
-                
-                connection.send(server, concat(msg, str.getBytes()), InetAddress.getByName("130.234.134.132"), 28070);
-                
-                while (true) {
-                    System.out.println(new String(connection.receive(server)));
-                }
-                
-            } catch (IOException ex) {
-                Logger.getLogger(RemoteConsole.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        JLabel prefix = new JLabel(" > ");
+        JTextField command = new JTextField();
+        command.addActionListener(x -> { 
+            area.append("> "+command.getText() + "\n");
+            controller.send(command.getText(), new String(password.getPassword()), ip.getText(), Integer.parseInt(port.getText()));
+            command.setText("");
         });
         
-        panel.add(button);
-        return panel;
+        bottom.add(prefix, BorderLayout.WEST);
+        bottom.add(command, BorderLayout.CENTER);
+        return bottom;
     }
     
+    public JPanel createTextArea() {
+        JPanel center = new JPanel(new BorderLayout());
+        
+        area = new JTextArea("Console\n-------\n\n");       
+        JScrollPane scrollPane = new JScrollPane(area);
+        area.setEditable(false);
+        area.setFont(new Font("monospaced", Font.PLAIN, 12));
+        area.setBackground(Color.black);
+        area.setForeground(Color.yellow);
+        
+        center.add(scrollPane, BorderLayout.CENTER);
+        return center;
+    }
     
-    public byte[] concat(byte[] a, byte[] b) {
-        int aLen = a.length;
-        int bLen = b.length;
-        byte[] c= new byte[aLen+bLen];
-        System.arraycopy(a, 0, c, 0, aLen);
-        System.arraycopy(b, 0, c, aLen, bLen);
-        return c;
-     }
+    public JPanel createTopBar(String withIp, String withPort) {
+        JPanel top = new JPanel(new BorderLayout());
+        
+        JPanel panel = new JPanel();
+        JLabel label1 = new JLabel("IP:");
+        ip = new JTextField(15);
+        ip.setText(withIp == null ? "" : withIp);
+        JLabel label2 = new JLabel("  Port:");
+        port = new JTextField(5);
+        port.setText(withPort == null ? "" : withPort);
+        JLabel label3 = new JLabel("  Password:");
+        password = new JPasswordField(10);
+        JCheckBox remember = new JCheckBox("Remember password");
+        
+        panel.add(label1);
+        panel.add(ip);
+        panel.add(label2);
+        panel.add(port);
+        panel.add(label3);
+        panel.add(password);
+        panel.add(remember);
+        
+        top.add(panel, BorderLayout.WEST);
+        return top;
+    }
 }
